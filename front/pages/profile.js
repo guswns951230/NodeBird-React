57df5a73e1e595baf14a053afd75a715f0989589
guswns951230 <rkdguswns0803@gmail.com>
2020-09-04 18:06:17 +0900

    Profile Page 만들기

--- front/pages/profile.js
+++ front/pages/profile.js
@@ -1,13 +1,23 @@
-import AppLayout from "../components/AppLayout";
 import Head from 'next/head';
 
+import AppLayout from '../components/AppLayout';
+import NicknameEditForm from '../components/NicknameEditForm';
+import FollowList from '../components/FollowList';
+
 const Profile = () => {
+  const followerList = [{ nickname: '강현준' }, { nickname: '븅신' }, { nickname: '빡대가리' }];
+  const followingList = [{ nickname: '강현준' }, { nickname: '븅신' }, { nickname: '빡대가리' }];
+
   return (
     <>
       <Head>
         <title>Node Bird | Profile</title>
       </Head>
-      <AppLayout><div>My Profile</div></AppLayout>
+      <AppLayout>
+        <NicknameEditForm />
+        <FollowList header="팔로잉 목록" data={followingList} />
+        <FollowList header="팔로워 목록" data={followerList} />
+      </AppLayout>
     </>
   );
 };

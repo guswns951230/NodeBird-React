cbbba44e72d368592c8cc749ab87955d58f5b9bc
guswns951230 <rkdguswns0803@gmail.com>
2020-09-08 19:50:10 +0900

    DummyData와 PostForm 만들기

--- front/reducers/post.js
+++ front/reducers/post.js
@@ -1,9 +1,54 @@
 export const initialState = {
-  mainPosts: [],
+  mainPosts: [{
+    id: 1,
+    User: {
+      id: 1,
+      nickname: 'qwui1',
+    },
+    content: '첫 번째 게시글 #해시태그 #익스프레스',
+    Images: [{
+      src: 'https://dimg.donga.com/wps/NEWS/IMAGE/2019/10/09/97794110.4.jpg',
+    }],
+    Comments: [{
+      User: {
+        nickname: 'nero',
+      },
+      content: '안녕하세요',
+    }, {
+      User: {
+        nickname: 'hero',
+      },
+      content: '반갑습니다',
+    }]
+  }],
+  imagePaths: [],
+  postAdded: false,
 }
+
+const ADD_POST = 'ADD_POST';
+export const addPost = {
+  type: ADD_POST,
+}
+
+const dummyPost = {
+  id: 2,
+  content: '더미데이터입니다.',
+  User: {
+    id: 1,
+    nickname: 'qwui2',
+  },
+  Images: [],
+  Comments: [],
+};
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case ADD_POST:
+      return {
+        ...state,
+        mainPosts: [dummyPost, ...state.mainPosts],
+        postAdded: true,
+      };
     default:
       return state;
   }

03ebe2a374836ab2d4ab8eeca914b48a8e9c8ba4
guswns951230 <rkdguswns0803@gmail.com>
2020-09-10 17:15:48 +0900

    saga 설치하고 generator 이해하기

--- front/store/configureStore.js
+++ front/store/configureStore.js
@@ -1,9 +1,10 @@
 import { createWrapper } from 'next-redux-wrapper';
 import { applyMiddleware, createStore, compose } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import thunkMiddleware from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
 
 import reducer from '../reducers';
+import rootSaga from '../sagas';
 
 const loggerMiddleware = ({ dispatch, getState }) => (next) => (action) => {
   console.log(action);
@@ -11,11 +12,13 @@ const loggerMiddleware = ({ dispatch, getState }) => (next) => (action) => {
 }
 
 const configureStore = () => {
-  const middlewares = [thunkMiddleware, loggerMiddleware]; // redux의 기능을 향상 시켜주는 역할
+  const sagaMiddleware = createSagaMiddleware();
+  const middlewares = [sagaMiddleware, loggerMiddleware]; // redux의 기능을 향상 시켜주는 역할
   const enhancer = process.env.NODE_ENV === 'production'
     ? compose(applyMiddleware(...middlewares))
     : composeWithDevTools(applyMiddleware(...middlewares))
   const store = createStore(reducer, enhancer);
+  store.sagaTask = sagaMiddleware.run(rootSaga);
   return store;
 };
 

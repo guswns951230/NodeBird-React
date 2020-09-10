4ba681cc755b6ee5260b14158b52ee41080c56a1
guswns951230 <rkdguswns0803@gmail.com>
2020-09-10 16:48:03 +0900

    redux-thunk 이해하기

--- front/reducers/user.js
+++ front/reducers/user.js
@@ -5,16 +5,57 @@ export const initialState = {
   loginData: {},
 }
 
-// action creator
 export const loginAction = (data) => {
-  return {
-    type: 'LOG_IN',
+  return (dispatch, getState) => {
+    const state = getState();
+    dispatch(loginRequestAction());
+    axios.get('/api/login')
+      .then((res) => {
+        dispatch(loginSuccessAction(res.data));
+      })
+      .catch((err) => {
+        dispatch(loginFailureAction(err));
+      })
   }
 }
 
-export const logoutAction = (data) => {
+// action creator
+export const loginRequestAction = (data) => {
   return {
-    type: 'LOG_OUT',
+    type: 'LOG_IN_REQUEST',
+    data,
+  }
+}
+
+export const loginSuccessAction = (data) => {
+  return {
+    type: 'LOG_IN_SUCCESS',
+    data,
+  }
+}
+
+export const loginFailureAction = (data) => {
+  return {
+    type: 'LOG_IN_FAILURE',
+    data,
+  }
+}
+
+export const logoutRequestAction = (data) => {
+  return {
+    type: 'LOG_OUT_REQUEST',
+  }
+}
+
+export const logoutSuccessAction = (data) => {
+  return {
+    type: 'LOG_OUT_SUCCESS',
+  }
+}
+
+export const logoutFailureAction = (data) => {
+  return {
+    type: 'LOG_OUT_FAILURE',
   }
 }
 
